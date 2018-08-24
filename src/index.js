a003a03d75cecc2d4d47623af8e911c1ef13cec8
Artem Ivanets <a.ivanets@lovata.com>
2018-08-24 18:46:52 +0300

    Fix gon

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
+import gon from 'gon';
 import React from 'react';
-import ReactDom from 'react-dom';
+import { render } from 'react-dom';
 import App from './app/App';
 
 const container = document.querySelector('#chat');
-const { channels } = window.gon;
-ReactDom.render(<App channels={channels} />, container);
+const { channels } = gon;
 
-// import faker from 'faker';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
-// if (process.env.NODE_ENV !== 'production') {
-//   localStorage.debug = 'chat:*';
-// }
+render(<App channels={channels} />, container);
